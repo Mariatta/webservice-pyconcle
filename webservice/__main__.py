4f359c22ea454e948b55411be020c4e28be46205
Mariatta Wijaya <mariatta@python.org>
2018-05-10 11:28:58 -0400

    Say thanks when a PR has been merged.

--- webservice/__main__.py
+++ webservice/__main__.py
@@ -19,7 +19,15 @@ async def issue_opened_event(event, gh, *args, **kwargs):
         'body': message,
     })
 
-
+@router.register("pull_request", action="closed")
+async def pr_closed_event(event, gh, *args, **kwargs):
+    """When a PR has been closed, say thanks"""
+    user = event.data["pull_request"]["user"]["login"]
+    is_merged = event.data["pull_request"]["merged"]
+    url = event.data["pull_request"]["comments_url"]
+    if is_merged:
+        message = f"Thanks for the PR @{user}"
+        await gh.post(url, data={"body": message})
 
 async def main(request):
     # read the GitHub webhook payload
